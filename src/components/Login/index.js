edfc33762233b8c57cc9d875d56b9d598e21d417
Abdelrhman Saleh <abdelrhmansaleh25@gmail.com>
2022-06-27 20:35:10 +0200

    third commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,21 +3,35 @@ import classes from "./index.module.css";
 import TextField from "@mui/material/TextField";
 import { Button, IconButton } from "@mui/material";
 
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [type, setType] = useState("password");
-  const handleTogglePassword = () => {
-    setShowPassword((showPassword) => !showPassword);
-    if (type === "password") {
-      setType("text");
-    } else {
-      setType("password");
-    }
+  const [values, setValues] = useState({
+    password: "",
+
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
   const theme = createTheme({
@@ -36,9 +50,17 @@ const Login = () => {
     <div className={classes.container}>
       <div className={classes.logo}></div>
       <div className={classes.form}>
-        <h2 className={classes.header}>LOGIN</h2>
-        <TextField id="outlined-basic" label="UserName" variant="outlined" />
+        <div>
+          <h2 className={classes.header}>LOGIN</h2>
+          <div className={classes.underline}></div>
+        </div>
         <TextField
+          color="secondary"
+          id="outlined-basic"
+          label="UserName"
+          variant="outlined"
+        />
+        {/* <TextField
           className={classes.password}
           name="password"
           type={type}
@@ -55,7 +77,33 @@ const Login = () => {
             {!showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
           </IconButton>
           <h5 className={classes.forgetText}>Forget Password</h5>
-        </div>
+        </div> */}
+        <FormControl variant="outlined">
+          <InputLabel color="secondary" htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            color="secondary"
+            id="outlined-adornment-password"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                  color="secondary"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
 
         <ThemeProvider theme={theme}>
           <Button variant="contained" color="primary">
